e04de0406c6cf1ce9b1a9fcb59aedc4f949434a5
rafaelrubenstein <73216681+rafaelrubenstein@users.noreply.github.com>
2021-10-20 14:29:06 -0400

    Update Task.java

--- To-do app/src/Task.java
+++ To-do app/src/Task.java
@@ -6,7 +6,7 @@ public class Task {
     private String date;
     private final int Max_Size = 50;
     private boolean isDone;
-
+// constructor for Task object. user may only enter tasks that are less than 50 characters
     public Task (String taskIn, String dateIn){
         while (taskIn.length() > Max_Size ){
             System.out.println("task must be less than 50 characters");
@@ -18,27 +18,33 @@ public class Task {
         date = dateIn;
         isDone = false;
     }
+//     getter method for task
     public String getTask(){
         return task;
     }
+//     getter method for date
     public String getDate(){
         return date;
     }
-    public boolean setTask(String taskIn){
-        if (taskIn.length() > Max_Size ){
+//  method that allows user to set a task less than 50 characters, as of 10/20 commit this method is not used.
+    public void setTask(String taskIn){
+      while (taskIn.length() > Max_Size ){
             System.out.println("task must be less than 50 characters");
-        }else {
-            task = taskIn;
+            Scanner keyboard = new Scanner(System.in);
+            System.out.println("Please enter a new task");
+            taskIn = keyboard.nextLine();
         }
-        return false;
+        task = taskIn;
     }
+//    allows user to set the date of the task. will be used to allow user to change the date of task
     public void setDate(String dateIn){
         date = dateIn;
     }
-
+// allows user to set the task as done 
     public void setTaskCompleted(){
         isDone = true;
     }
+//    getter method for done  
     public Boolean getIsDone(){
         return isDone;
     }
